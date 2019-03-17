b1df1adff031220c74743deb1b43b720b3fcefb5
MAYTESI <mayte16inga@gmail.com>
2019-03-17 16:24:11 -0500

    subiendo cambios en Js para el llenado automatico de select y option

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
+selectIndicators = document.getElementById("lista");
+let arreglo = Funciones.arrayOfIndicators("PER");
+for (let x = 0; x < arreglo.length; x++) {
+    option=document.createElement("option");
+    option.value=arreglo[x];
+    option.text=arreglo[x];
+    selectIndicators.appendChild(option);
+};
+select = document.getElementById("Age-1");
+for (let i = 1960; i <= 2016; i++) {
+    option = document.createElement("option");
+    option.value = i;
+    option.text = i;
+    select.appendChild(option);
+};
+selectTwo = document.getElementById("Age-2");
+for (let n = 1961; n <= 2017; n++) {
+    option = document.createElement("option");
+    option.value = n;
+    option.text = n;
+    selectTwo.appendChild(option);
+};
 
 /*const mostrar=document.getElementById("datos");
 let abc=Funciones.arrayOfIndicators("PER");
