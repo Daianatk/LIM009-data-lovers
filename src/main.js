9928aa01ec9c1a37c92a2202a120b46373b9699d
MAYTESI <mayte16inga@gmail.com>
2019-03-17 15:56:26 -0500

    subiendo cambios

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-/* Manejo del DOM */
+
+/*const mostrar=document.getElementById("datos");
+let abc=Funciones.arrayOfIndicators("PER");
+mostrar.innerHTML=abc;
+
+const showSection=document.getElementById("dta_ages");
+let showData=Funciones.arrayOfdata("MEX","Empleo de tiempo parcial, mujeres (% del total de mujeres empleadas)");
+showSection.innerHTML=showData;*/
